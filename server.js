3b2b581dc4e46eaa8e9f508dd61fa881a8ffb82e
Justin Joy <JJ@Dev-Team.Dlink>
2018-06-30 23:26:43 +0530

    Heroku based config changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 var app = express();
-var port = 3001;
+const port = process.env.PORT || 3001;
 var maintenanceMode = 0;
 
 hbs.registerPartials(__dirname + '/views/partials');
